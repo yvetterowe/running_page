f89f45e7a8cfca8c1c5eeaa394554edc2c268178
yvetterowe <yvetterowe1116@gmail.com>
2023-03-28 23:19:33 -0400

    customize stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,14 @@
 module.exports = {
   pathPrefix: process.env.PATH_PREFIX || '/',
   siteMetadata: {
-    siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
+    siteTitle: '🏃🏻‍♀️',
+    siteUrl: 'https://run.haoluo.io',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-    description: 'Personal site and blog',
+    description: 'Hao\'s running journey',
     navLinks: [
       {
-        name: 'Blog',
-        url: 'https://yihong.run/running',
-      },
-      {
         name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        url: 'https://haoluo.io',
       },
     ],
   },
--- src/utils/const.js
+++ src/utils/const.js
@@ -1,6 +1,6 @@
 // const
 const MAPBOX_TOKEN =
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+  'pk.eyJ1IjoieXZldHRlcm93ZSIsImEiOiJjbGZ0NDQyeWswYzh6M3ZyeWt5aTNhOHI4In0.6zCzKvjZhXBcndnsu7hQGg';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
@@ -25,7 +25,7 @@ const MAP_HEIGHT = 600;
 const ROAD_LABEL_DISPLAY = true;
 
 // IF you outside China please make sure IS_CHINESE = false
-const IS_CHINESE = true;
+const IS_CHINESE = false;
 const USE_ANIMATION_FOR_GRID = false;
 const CHINESE_INFO_MESSAGE = (yearLength, year) => {
   const yearStr = year === 'Total' ? '所有' : ` ${year} `;
